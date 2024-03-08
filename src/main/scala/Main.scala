147af2ed943f2dc96dec29b2dd2cbe4f05d8a839
Facundo Mastricchio <facundomastricchio@gmail.com>
2024-03-07 22:04:41 -0300

    Add AES constants

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,8 @@
+import aes_cipher.Constants
+
 @main def hello(): Unit =
   println("Hello world!")
+  println(Constants.R_CON.mkString("Array(", ", ", ")"))
   println(msg)
 
 def msg = "I was compiled by Scala 3. :)"
