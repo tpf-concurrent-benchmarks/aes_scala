cf2500c6d48ff8b11c3497920340787ffbf84fac
ArmandoCivini <tcivini@gmail.com>
2024-03-07 22:18:25 -0300

    added constants

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,11 @@
+import constants._
+
 @main def hello(): Unit =
   println("Hello world!")
   println(msg)
+  // Convert each byte to its hexadecimal representation and concatenate them
+  val hexString: String = S_BOX.map(byte => f"${byte & 0xFF}%02x, ").mkString
 
+  // Print the hexadecimal string
+  println(hexString)
 def msg = "I was compiled by Scala 3. :)"
