98368764cc5829794c126992bb354d076124ff4d
Facundo Mastricchio <facundomastricchio@gmail.com>
2024-03-11 21:59:46 -0300

    Add ChunkWriter

---
+++ src/main/scala/utils/ChunkWriter.scala
@@ -0,0 +1,44 @@
+package utils
+
+import java.io.{BufferedOutputStream, OutputStream}
+
+class ChunkWriter (output: OutputStream, removePadding: Boolean) {
+  private val bufferedOutput = new BufferedOutputStream(output)
+
+  def writeChunks(chunks: Array[Array[Byte]]): Either[Exception, Unit] = {
+    try {
+      for (chunk <- chunks) {
+        writeChunk(chunk)
+      }
+      Right(())
+    } catch {
+      case e: Exception => Left(e)
+    }
+  }
+
+  private def writeChunk(chunk: Array[Byte]): Unit = {
+    if (removePadding) {
+      writeChunkWithoutPadding(chunk)
+    } else {
+      bufferedOutput.write(chunk)
+    }
+  }
+
+  private def writeChunkWithoutPadding(chunk: Array[Byte]): Unit = {
+    val paddingPos = chunk.indexWhere(_ == 0) match {
+      case -1 => chunk.length
+      case pos => pos
+    }
+    bufferedOutput.write(chunk, 0, paddingPos)
+  }
+
+  def close(): Unit = {
+    bufferedOutput.flush()
+    bufferedOutput.close()
+  }
+}
+
+object ChunkWriter {
+  def apply(output: OutputStream, removePadding: Boolean): ChunkWriter =
+    new ChunkWriter(output, removePadding)
+}
